402c301cafa2f96d4fedd4908e715ce1a7ac0daf
Vivek Sasidharan <mail@viveks.dev>
2021-06-01 23:43:49 +0530

    Benchmarks added

---
+++ DotKsuid.Benchmarks/KsuidGuidBenchmarks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace DotKsuid.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class KsuidPerfTests
+    {
+        [Benchmark]
+        public Ksuid Ksuids() => Ksuid.NewKsuid();
+
+        [Benchmark]
+        public Guid Guids() => Guid.NewGuid();
+
+        [Benchmark]
+        public KSUID.Ksuid KsuidNet() => KSUID.Ksuid.Generate();
+
+
+        [Benchmark]
+        public string KsuidString() => Ksuid.NewKsuid().ToString();
+
+        [Benchmark]
+        public string GuidString() => Guid.NewGuid().ToString();
+
+        [Benchmark]
+        public string KsuidNetString() => KSUID.Ksuid.Generate().ToString();
+    }
+}
---
+++ DotKsuid.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace DotKsuid.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BenchmarkRunner.Run<KsuidPerfTests>();
+        }
+    }
+
+}
